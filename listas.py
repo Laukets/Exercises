21f17b0852a4290800eefedb8581752177cc5b78
Lauky <paula_calixto@outlook.com>
2022-01-10 20:33:06 -0300

    pra nao sair da rotina

---
+++ listas.py
@@ -0,0 +1,35 @@
+num = [17, 123, 87, 34, 66, 8398, 44, 'gato', 'cachorro', [], [10, True], False]
+
+a = num[len(num)-1]
+
+b = num[7].upper()
+
+c = num[7][0] # retorna caracter no indice 0 da string no indice 7 da lista
+
+d = 57 in num # 57 é um item da sublista e nao da lista externa
+
+e = [0]*4
+f = num + [3.14, 3.65]
+
+g = id(num)
+
+num[1:3] = ['x', 'y'] # modifica os itens no intervalo indicado
+num[1:3] = [] # remove os intes no intervalo indicado
+num[4:4] = ['e'] # acrescenta sem substituir nenhum item
+
+del num[1] # deleta item indicado
+
+lista = [81,82,83]
+h = lista
+'''print(lista is h) # retorna True
+h[0] = 5
+print(lista) # retorna lista modificada'''
+
+i = lista[:]
+'''print(i is lista) # retorna False
+i[0] = 4
+print(i)
+print(lista)'''
+
+j = [num] * 3
+print(j)
